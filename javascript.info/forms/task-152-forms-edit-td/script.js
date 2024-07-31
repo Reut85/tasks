0e3e720b93acefde999e7095929c8c0d3f4ba6bd
reut85 <daryamylove2017@gmail.com>
2024-07-31 20:45:00 +0300

    Finally I made 152 task, congratulation for myself!

--- javascript.info/forms/task-152-forms-edit-td/script.js
+++ javascript.info/forms/task-152-forms-edit-td/script.js
@@ -2,46 +2,70 @@ let table = document.getElementById('bagua-table');
 
 /* ваш код */
 
+
+class Textarea {
+    constructor(text) {
+        this.text = text;
+    }
+
+    createTextarea() {
+        const form = document.createElement("form");
+        const area = document.createElement("textarea");
+        area.value = this.text;
+        form.append(area);
+        return form;
+    }
+}
+
 class Button {
     constructor(text) {
         this.text = text;
     }
     createButton() {
-        const buttonElement = document.createElement("p");
-        buttonElement.innerText = this.text;
-        buttonElement.classList.add('button');
-        //buttonElement.style.display = "inline"; // Задайте бажану позицію (наприклад, "inline", "block" і т. д.)
+        let buttonElement = document.createElement("button");
+        buttonElement.innerHTML = this.text;
         return buttonElement;
     }
 }
 
-const leftButton = new Button('ЗГОДА');
-const rightButton = new Button('ВІДМІНА');
+let leftButton;
+let rightButton;
 let selectedTd;
 
 table.onclick = function (event) {
     let target = event.target; // де відбувся клік?
 
     if (target.tagName != 'TD') return; // не на TD? Тоді нас не цікавить
+    const cashText = target.innerHTML;
+    highlight(target); // замінити td на textarea
 
-    highlight(target); // виділити TD
-};
-
-// todo
+    leftButton = new Button('ЗГОДА').createButton();
+    rightButton = new Button('ВІДМІНА').createButton();
+    leftButton.classList.add('buttonLeft');
+    rightButton.classList.add('buttonRight');
+    leftButton.setAttribute('type', 'left');
+    rightButton.setAttribute('type', 'right');
+    target.appendChild(leftButton);
+    target.appendChild(rightButton);
+    target.onclick = function (event) {
+        let subject = event.target;
+        if (subject.getAttribute('type') == 'left') {
+            target.innerHTML = selectedTd.children[0].value;
+            selectedTd = '';
+            leftButton = '';
+            rightButton = '';
+        }
+        else if (subject.getAttribute('type') == 'right') {
+            target.innerHTML = cashText;
+            selectedTd = '';
+            leftButton = '';
+            rightButton = '';
+        }
+    }
+}
 function highlight(td) {
-    if (selectedTd) { // видалити наявне виділення, якщо таке є
-        selectedTd.classList.remove('editHTML');
-        selectedTd.innerHTML = selectedTd.textContent;
-    }
-    selectedTd = td;
-    selectedTd.classList.add('editHTML'); // виділити новий td для редагування
-
-    function addLineBreakBeforeBR(text) {
-        // Замінюємо всі теги <br> на перенос рядка та <br>
-        let newText = text.split('<br>').join('\n<br>');
-        return newText;
-    }
-
-    selectedTd.textContent = addLineBreakBeforeBR(selectedTd.innerHTML);
-    //alert(selectedTd.innerHTML)
+    selectedTd = new Textarea(td.innerHTML).createTextarea();
+    selectedTd.children[0].classList.add('editHTML');
+    td.textContent = '';
+    td.append(selectedTd);
 }
